79ea6cf1cd945443fd84bf1474bad9a45d842d54
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-05-25 16:55:27 +0430

    added head

---
+++ pages/_app.tsx
@@ -0,0 +1,53 @@
+import App from "next/app";
+import Head from "next/head";
+import Layout from "../components/layout";
+
+class CustomApp extends App {
+  static async getInitialProps(prop: any) {
+    let pageProps = {};
+
+    if (prop.Component.getInitialProps) {
+      pageProps = await prop.Component.getInitialProps(prop.ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <div>
+        <Head>
+          <title>Chat Prop</title>
+          <meta name="description" content="Advance chat room" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css"
+          />
+        </Head>
+        <div>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+        <style global jsx>{`
+          * {
+            margin: 0px;
+            padding: 0px;
+          }
+          body {
+            background-color: #282c34;
+            width: 100%;
+            min-height: 100vh;
+            color: white;
+            font-size: 16px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default CustomApp;
