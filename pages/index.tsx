e40f30b59590d92d03162a85855263291de94ae4
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-05-26 00:51:22 +0430

    changed content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-
-import "../style/index.css";
+import {Home} from '../components/Home';
 
 class Index extends React.Component<{}, {}> {
   render() {
-    return <div>hello</div> ;
+    return <Home /> ;
   }
 }
 
